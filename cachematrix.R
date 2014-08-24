21f5eb0a2d97cf3a3fe0d800586596ab8abf994a
KingStorm <hduyejianhao@163.com>
2014-08-24 16:38:31 +0800

    Revert "my work to make a cachematrix"
    This reverts commit 5115505536a07c5f910c6be8c24257703cb04051.

--- cachematrix.R
+++ cachematrix.R
@@ -1,84 +1,15 @@
-## Put comments here that give an overall description
-
-## of what your functions do
-
-
+## Put comments here that give an overall description of what your
+## functions do
 
 ## Write a short comment describing this function
 
+makeCacheMatrix <- function(x = matrix()) {
 
-
-makeCacheMatrix <- function(m = matrix()) {
-  
-  i <- NULL
-  
-  
-  
-  set <- function(t){
-    
-    m <<- t
-    
-    i <<- NULL
-    
-  }
-  
-  
-  
-  get <- function() {m}
-  
-  
-  
-  setInverse <- function(inverse){
-    
-    i <<- inverse
-    
-  }
-  
-  
-  
-  getInverse <- function() {i
-                            
-  }
-  
-  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
-  
 }
 
 
-
-
-
 ## Write a short comment describing this function
-
-
 
 cacheSolve <- function(x, ...) {
-  
-  i <- x$getInverse()
-  
-  
-  
-  if(!is.null(i)){
-    
-    message("getting the cached data")
-    
-    return(i)
-    
-  }
-  
-  
-  
-  m <- x$get()
-  
-  inverse <- solve(m)
-  
-  x$setInverse(inverse)
-  
-  
-  
-  inverse
-  
-  ## Return a matrix that is the inverse of 'x'
-  
+        ## Return a matrix that is the inverse of 'x'
 }
-
